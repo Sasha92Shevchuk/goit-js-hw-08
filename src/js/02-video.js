7445e0825d885047cb377626d7e137341241ed63
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2022-11-28 12:47:19 +0200

    done 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const saveTime = localStorage.getItem('videoplayer-current-time');
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onSaveTimeHandler = function (data) {
+  //   console.log('data!', data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.on('timeupdate', throttle(onSaveTimeHandler, 1000));
+
+player.setCurrentTime(saveTime);
