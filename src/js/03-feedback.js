6d611d11d272a6470354334ab406de6ce3e1aeb4
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2022-11-29 09:51:54 +0200

    done 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,27 +4,29 @@ const formEl = document.querySelector('.feedback-form');
 const inputEl = document.querySelector('.feedback-form input');
 
 const textAreaEl = document.querySelector('.feedback-form textarea');
-// const submit = document.querySelector('.feedback-form button');
 
 const STORAGE_KEY = 'feedback-form-state';
 const getDataForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
-// const parsedGetDataForm = JSON.parse(getDataForm);
-// console.log(getDataForm);
 
 formEl.addEventListener('input', throttle(onIputHandler, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
 updateFieldsForm();
-
+let formFields = {};
 function onIputHandler(e) {
-  const { email, message } = e.currentTarget.elements;
-  const formFields = {
-    email: email.value,
-    message: message.value,
-  };
-
+  formFields[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formFields));
 }
+
+// function onIputHandler(e) {
+//   const { email, message } = e.currentTarget.elements;
+//   const formFields = {
+//     email: email.value,
+//     message: message.value,
+//   };
+
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formFields));
+// }
 
 function updateFieldsForm() {
   if (getDataForm) {
